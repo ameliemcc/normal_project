b284f036af129d55224b28b6194f7f0d2827eee5
Tinto13 <61778236+Tinto13@users.noreply.github.com>
2020-04-10 13:38:09 +0200

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+// Commentaire d'essai fait par Tinto
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
