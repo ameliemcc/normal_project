d7693356ee12baf4b72f4adbef0cd0722cdfce6c
amélie <amelie.mccormick@unil.ch>
2020-05-05 22:44:38 +0200

    installation bootstrap

--- client/main.js
+++ client/main.js
@@ -2,11 +2,16 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 // Commentaire d'essai fait par Tinto
 import './main.html';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+</button>
 
 Template.hello.helpers({
   counter() {
