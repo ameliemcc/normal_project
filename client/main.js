1f415fe3dae37d9cae1df464e070375d62592bbc
amélie <amelie.mccormick@unil.ch>
2020-05-05 23:40:30 +0200

    Installation bootstrap
    j'ai installé les dernières versions de jquery, pooper.js et bootstrap et mis un lien vers le dossier CSS bootstrap dans main.js mais je n'arrive pas à faire fonctionner les components bootstrap sans le lien CDN

--- client/main.js
+++ client/main.js
@@ -1,14 +1,17 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 // Commentaire d'essai fait par Tinto
-import './main.html';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+import './main.html';
 
 
-Template.hello.onCreated(function helloOnCreated() {
+
+/*Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
+
 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
   <span aria-hidden="true">&times;</span>
 </button>
@@ -24,4 +27,4 @@ Template.hello.events({
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
-});
+});*/
