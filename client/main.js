272840fbbded5a4d5e568bccf0ec13955c5fec5a
Tinto13 <61778236+Tinto13@users.noreply.github.com>
2020-05-06 08:32:37 +0200

    Delete NUATests
    Clean-up tests directory

--- client/main.js
+++ client/main.js
@@ -1,6 +1,9 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 // Commentaire d'essai fait par Tinto
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
