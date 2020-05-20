bcad1636c4d1d27814c20bfa77ecfd4163d860df
Tinto13 <jacinto.fernandez@unil.ch>
2020-05-20 11:23:05 +0200

    Update template Mes Favoris
    Utillisation des templates et création de la base d'annonce

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,13 @@
 import { Template } from 'meteor/templating';
  
 import './body.html';
-
+import {Annonces} from '../api/annonces.js'
 Template.body.helpers({
   
   surname :[
     {text:"Jacinto"},
   ],
-  
+  annonces : function (){
+    return Annonces.find({}).fetch()
+  }
 });
