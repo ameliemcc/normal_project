dd5321a7dc3e9da5f0409f83b38423856df171b2
Tinto13 <jacinto.fernandez@unil.ch>
2020-05-20 11:51:33 +0200

    Ajout de variables Mes Favoris/Profils
    Ajout de variables pour définir les profils encore en local depuis surname, sera remplacé par les valeurs de la DB plus tard

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,7 +5,9 @@ import {Annonces} from '../api/annonces.js'
 Template.body.helpers({
   
   surname :[
-    {text:"Jacinto"},
+    {nom:"Jacinto Fernandez", age:"63 ans", profess:"Etudiant", localité: "Yverdon"},
+    {nom:"Pierre Dupont", age:"25 ans", profess: "Apprenti", localité: "Yens"},
+    {nom:"Paul Durand", age:"25 ans", profess: "Boulanger", localité: "Morrens"}
   ],
   annonces : function (){
     return Annonces.find({}).fetch()
