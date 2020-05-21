27952514b7e3378b5bb6209e7470c4139f258fe5
amélie <amelie.mccormick@unil.ch>
2020-05-22 01:34:14 +0200

    Implémentation de la création de comptes utilisateurs

--- imports/ui/body/body.js
+++ imports/ui/body/body.js
@@ -9,14 +9,18 @@ import '../templates/creer.js';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import { Template } from 'meteor/templating';
+import { Accounts } from 'meteor/accounts-base';
 
+Template.app_body.helpers({
+    utilisateur: () => Meteor.user().username,
+})
 
 Template.app_body.events({
     'click #accueil'(event){
         event.preventDefault();
         FlowRouter.go('accueil');
     },
-    'click #favoris'(event){
+    'click #favvoris'(event){
         event.preventDefault();
         FlowRouter.go('favoris');
     },
@@ -28,9 +32,57 @@ Template.app_body.events({
         event.preventDefault();
         FlowRouter.go('login');
     },
+    'click #logout'(event){
+        event.preventDefault();
+    Meteor.logout();
+    },
     'click #creation'(event){
         event.preventDefault();
         FlowRouter.go('creation');
+    },
+    
+    'click #submit'(event) {
+        event.preventDefault();
+        //let nom = document.getElementById('nom').value;
+       // let prenom = document.getElementById('prenom').value;
+        let email = document.getElementById('inputEmail4').value;
+        let mdp = document.getElementById('inputPassword4').value;
+        let mdpCheck = document.getElementById('inputPasswordCheck').value;
+       // let localite = document.getElementById('localite').value;
+      //  let npa = document.getElementById('npa').value;
+        let profession = document.getElementById('profession').value;
+        let sexe = document.getElementById('sexe').value;
+      //  let date = document.getElementById('example-date-input').value;
+       // let info = document.getElementById('exampleFormControlTextarea1').value;
+      //  let url = document.getElementById('basic-url').value;
+        if (mdp.length > 4){
+            if(mdp == mdpCheck){
+                if(email != '' && mdp != '' && profession != '' && sexe != ''){
+                    Accounts.createUser({
+                        username: email,
+                        password: mdp,
+                        profile:{
+                            profession: profession,
+                            sexe: sexe
+                        },
+                    }, 
+                    (error) => {
+                        if (error) {
+                            alert(error.message);
+                        } else {
+                            setTimeout(() => FlowRouter.go('accueil'), 200);
+                        }
+                    }
+                );
+                } else{
+                    alert('Veuillez remplir tous les champs requis.');
+                }
+            }else{
+             alert('Veuillez reconfirmer votre mot de passe.');   
+            }
+        }else{
+            alert('Votre mot de passe est trop court! Veuillez choisir un mot de passe de minimum 5 caractères. ');
+        }
     },
 
 });
