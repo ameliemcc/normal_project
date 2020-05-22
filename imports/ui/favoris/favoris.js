aa5e06cc744bc5eda496335d7a39e91b80236330
Tinto13 <jacinto.fernandez@unil.ch>
2020-05-22 16:33:25 +0200

    Ajout Base de données
    AJout de la base

--- imports/ui/favoris/favoris.js
+++ imports/ui/favoris/favoris.js
@@ -1,1 +1,18 @@
+import { Template } from 'meteor/templating';
+import {Annonces} from '../../api/annonces.js'
+
+import '../templates/annonce_grid.html'
+import '../templates/profils_grid.html'
 import './favoris.html'
+
+Template.favoris.helpers({
+  
+  surname :[
+    {nom:"Jacinto Fernandez", age:"63 ans", profess:"Etudiant", localité: "Yverdon"},
+    {nom:"Pierre Dupont", age:"25 ans", profess: "Apprenti", localité: "Yens"},
+    {nom:"Paul Durand", age:"25 ans", profess: "Boulanger", localité: "Morrens"}
+  ],
+  annonces : function (){
+    return Annonces.find({}).fetch()
+  }
+});
