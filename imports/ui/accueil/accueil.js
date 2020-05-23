9b61659f9d5dac23023162544dd4140e2253c065
Tinto13 <jacinto.fernandez@unil.ch>
2020-05-23 22:07:24 +0200

    Mise à jour copie de Master
    Création de la new brnache TintoNew et copie du Master de Tinto actuel

--- imports/ui/accueil/accueil.js
+++ imports/ui/accueil/accueil.js
@@ -1,2 +1,21 @@
+import { Template } from 'meteor/templating';
+import {Annonces} from '../../api/annonces.js';
+/*import { Membres } from "../../api/membres.js";*/
+
+import '../templates/nannonce_grid.html';
+import '../templates/nprofils_grid.html';
 import './accueil.html';
-import { Membres } from "../../api/membres.js";
+
+
+Template.accueil.helpers({
+  
+  surname :[
+    {nom:"Jacinto Fernandez", age:"63 ans", profess:"Etudiant", localité: "Yverdon"},
+    {nom:"Pierre Dupont", age:"25 ans", profess: "Apprenti", localité: "Yens"},
+    {nom:"Paul Durand", age:"25 ans", profess: "Boulanger", localité: "Morrens"},
+    {nom:"Gavin Vinasi", age:"25 ans", profess: "Pâtissier", localité: "Leysin"}
+  ],
+  annonces : function (){
+    return Annonces.find({}).fetch()
+  }
+});
