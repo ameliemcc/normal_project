015c3c586575b504fe6447b79e28224c5d637ec7
rebeccakneubuehler <61821829+rebeccakneu@users.noreply.github.com>
2020-05-24 00:45:04 +0200

    1

--- imports/ui/accueil/accueil.js
+++ imports/ui/accueil/accueil.js
@@ -1,12 +1,4 @@
 import './accueil.html';
 import { Membres } from "../../api/membres.js";
 
-/* Code pour forcer l'utilisateur à se connecter avant d'accéder au site 
-if (currentUser = true) {
-    event.preventDefault();
-    FlowRouter.go('accueil')
-} else {
-    event.preventDefault();
-    FlowRouter.go('1')
-} 
-*/
+
