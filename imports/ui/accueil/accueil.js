1c1806cae87c67b55753cf59bae11598769849fc
amélie <amelie.mccormick@unil.ch>
2020-05-23 22:43:32 +0200

    page 1

--- imports/ui/accueil/accueil.js
+++ imports/ui/accueil/accueil.js
@@ -1,6 +1,7 @@
 import './accueil.html';
 import { Membres } from "../../api/membres.js";
 
+<<<<<<< Updated upstream
 Template.accueil.helpers({
   "Membres": () => Membres.find({}).fetch()
 });
@@ -14,3 +15,15 @@ Template.accueil.helpers({
     { text: 'This is task 3' },
   ],
 });
+=======
+
+/* Code pour forcer l'utilisateur à se connecter avant d'accéder au site 
+if (currentUser = true) {
+    event.preventDefault();
+    FlowRouter.go('accueil')
+} else {
+    event.preventDefault();
+    FlowRouter.go('1')
+} 
+*/
+>>>>>>> Stashed changes
