2bfa9d57ff0d1e932c368b28d3d5b3cb8b013109
Ga4646 <62051263+Ga4646@users.noreply.github.com>
2020-05-24 20:09:18 +0200

    afficher membre

--- imports/ui/templates/creer.js
+++ imports/ui/templates/creer.js
@@ -1,1 +1,36 @@
 import './creer.html';
+import { Membres } from '../../api/membres';
+
+// eslint-disable-next-line no-undef
+/* Template.creer.events({
+  'submit .new-membre'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const nom = event.target.nom.value;
+    const prenom = event.target.prenom.value;
+    // let age = event.target.age.value
+    const prof = event.target.profession.value;
+    const localite = event.target.local.value;
+    const code = event.target.code.value;
+
+    // Insert a task into the collection
+    Membres.insert({
+      nom,
+      prenom,
+      prof,
+      localite,
+      code,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.nom.value = '';
+    target.prenom.value = '';
+    target.profession.value = '';
+    target.local.value = '';
+    target.code.value = '';
+  },
+
+}); */
