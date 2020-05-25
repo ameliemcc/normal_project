65a7495866dc130c1f5dfa358b41123b2e7d6d61
Ga4646 <62051263+Ga4646@users.noreply.github.com>
2020-05-25 16:15:12 +0200

    recupérer données compte utilisateurs

--- imports/ui/favoris/favoris.js
+++ imports/ui/favoris/favoris.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Annonces } from '../../api/annonces.js';
 
@@ -9,20 +10,13 @@ import './favoris.html';
 
 Template.favoris.helpers({
 
-  surname: [
-    {
-      nom: 'Jacinto Fernandez', age: '63 ans', profess: 'Etudiant', localité: 'Yverdon',
-    },
-    {
-      nom: 'Pierre Dupont', age: '25 ans', profess: 'Apprenti', localité: 'Yens',
-    },
-    {
-      nom: 'Paul Durand', age: '25 ans', profess: 'Boulanger', localité: 'Morrens',
-    },
-  ],
   annonces () {
     return Annonces.find({}).fetch();
   },
 
-});
+  utilisateurs () {
+    return Meteor.users.find({}).fetch();
+  },
 
+
+});
