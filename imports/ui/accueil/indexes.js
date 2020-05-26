69547264bf033433730fad406ee6798f9ce6ce54
rebeccakneubuehler <61821829+rebeccakneu@users.noreply.github.com>
2020-05-26 17:21:33 +0200

    Update indexes.js

--- imports/ui/accueil/indexes.js
+++ imports/ui/accueil/indexes.js
@@ -1,10 +1,14 @@
-/* Pour la barre de recherche */
+/* Pour la barre de recherche 
+Il faut que l'utilisateur puisse entrer une valeur. En cliquant sur le bouton rechercher, 
+cette valeur doit être comparée aux valeurs dans la base de données. 
+Ensuite, seules les valeurs qui sont égales aux valeurs entrées doivent s'afficher. 
+*/
 
 import {Index, MinimongoEngine} from 'meteor/easy:search'
 
 const membresIndex = new Index({
-    collection: membres;
-    fields: ['','']
+    collection: membres,
+    fields: ['',''],
 })
 
 
