69cb2cd0b36e70e66d6c32ce4ea210a636fabec0
Ga4646 <62051263+Ga4646@users.noreply.github.com>
2020-05-28 19:54:24 +0200

    test recupérer la valeur dans accueil

---
+++ imports/ui/templates/accprofils_grid.js
@@ -0,0 +1,12 @@
+import './accprofils_grid.html';
+import { Annonces } from '../../api/annonces';
+
+Template.newmemb.events({
+  'click .ajoutay'() {
+    const prof = this._id;
+    // eslint-disable-next-line no-unused-expressions
+    Annonces.insert({
+      prof,
+    });
+  },
+});
