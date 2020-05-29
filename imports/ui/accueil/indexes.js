80b344c764af4fe247ce9c2f28ad75946301d7da
Ga4646 <62051263+Ga4646@users.noreply.github.com>
2020-05-29 17:24:16 +0200

    Update indexes.js

--- imports/ui/accueil/indexes.js
+++ imports/ui/accueil/indexes.js
@@ -1,14 +1,21 @@
-/* Pour la barre de recherche 
-Il faut que l'utilisateur puisse entrer une valeur. En cliquant sur le bouton rechercher, 
-cette valeur doit être comparée aux valeurs dans la base de données. 
-Ensuite, seules les valeurs qui sont égales aux valeurs entrées doivent s'afficher. 
+/* Pour la barre de recherche
+Il faut que l'utilisateur puisse entrer une valeur. En cliquant sur le bouton rechercher,
+cette valeur doit être comparée aux valeurs dans la base de données.
+Ensuite, seules les valeurs qui sont égales aux valeurs entrées doivent s'afficher. */
 
+import { Index, MinimongoEngine } from 'meteor/easy:search';
+import { Annonces } from '../../api/annonces.js';
 
-import {Index, MinimongoEngine} from 'meteor/easy:search'
+const listeAnn = Annonces;
 
-const membresIndex = new Index({
-    collection: membres,
-    fields: ['',''],
-})
+const listAnnIndex = new Index({
+  collection: listeAnn,
+  fields: ['name', 'score'],
+  engine: new MinimongoEngine(),
+});
 
-*/
+Tracker.autorun(function () {
+  const cursor = listAnnIndex.search('Marie');
+  console.log(cursor.fetch());
+  console.log(cursor.count());
+});
