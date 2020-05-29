bde42e70c4c6931d66f59b75379e09a2a5032ee5
Tinto13 <jacinto.fernandez@unil.ch>
2020-05-29 21:44:42 +0200

    Update page accueil avec click favoris
    Ajout id des annonces et des profils "likés" sou sforme de tableau d'index dans la collection users

--- imports/ui/templates/accannonce_grid.js
+++ imports/ui/templates/accannonce_grid.js
@@ -1,14 +1,17 @@
 import { Meteor } from 'meteor/meteor';
-import { ReactiveVar } from 'meteor/reactive-var';
 import { Annonces } from '../../api/annonces';
 import './accannonce_grid.html';
 
 Template.newann.events({
   'click .ajoutay1'() {
-    const profcur = this._id;
-    let curr = Meteor.userId();
+    let profcur = this._id;
+    let currmemb = Meteor.userId();
     alert(profcur);
-    alert(curr);
+    alert(currmemb);
+    Meteor.users.update(
+        {_id: Meteor.userId() },
+        {$push: {"profile.fav_annonce": this._id}}
+      )
     },
 
-  })
+})
