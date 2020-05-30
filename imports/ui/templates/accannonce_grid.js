232f7d4b26ee8e812b93c478620debe6e7ee801a
Tinto13 <jacinto.fernandez@unil.ch>
2020-05-30 14:36:43 +0200

    Update page Favoris
    Trapper si c'est un nuveau profil et pusher fav_annonce et fav_personne pour éviter une erreur due au undefined status

--- imports/ui/templates/accannonce_grid.js
+++ imports/ui/templates/accannonce_grid.js
@@ -5,7 +5,14 @@ import './accannonce_grid.html';
 Template.newann.events({
   
     'click.ajoutay1'() {
-      if (Meteor.user().profile.fav_annonce.includes(this._id)){
+      if (typeof Meteor.user().profile.fav_annonce === 'undefined') {
+          Meteor.users.update({_id: Meteor.userId() },
+          {$push: {"profile.fav_annonce": this._id}}
+          );
+  
+          alert("Votre annonce est maintenant likée");
+      }   
+      else if (Meteor.user().profile.fav_annonce.includes(this._id)){
         alert("Vous avez déjà liké cette annonce");
   
       }  else {
--- imports/ui/templates/accprofils_grid.js
+++ imports/ui/templates/accprofils_grid.js
@@ -5,7 +5,14 @@ import './accprofils_grid.html';
 
 Template.newmemb.events({
   'click.ajoutay'() {
-    if (Meteor.user().profile.fav_personne.includes(this._id)){
+    if (typeof Meteor.user().profile.fav_personne === 'undefined') {
+      Meteor.users.update({_id: Meteor.userId() },
+      {$push: {"profile.fav_personne": this._id}}
+      );
+
+      alert("Votre profil est maintenant likée");
+    }
+    else if (Meteor.user().profile.fav_personne.includes(this._id)){
       alert("Vous avez déjà liké cette personne!");
 
     }  else {
