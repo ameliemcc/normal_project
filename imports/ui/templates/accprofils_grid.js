94b560b6d8381c1c2750f9cf13a2601709e51933
Tinto13 <jacinto.fernandez@unil.ch>
2020-05-30 12:33:03 +0200

    Update Accueil
    Teste qu'une personne n'a pas encore été likée et retourne un message d'erreur si déjà liké

--- imports/ui/templates/accprofils_grid.js
+++ imports/ui/templates/accprofils_grid.js
@@ -5,12 +5,18 @@ import './accprofils_grid.html';
 
 Template.newmemb.events({
   'click.ajoutay'() {
+    if (Meteor.user().profile.fav_personne.includes(this._id)){
+      alert("Vous avez déjà liké cette personne!");
+
+    }  else {
+        Meteor.users.update({_id: Meteor.userId() },
+        {$push: {"profile.fav_personne": this._id}}
+        );
+
+        alert("Votre profil est maintenant liké");
+
+    }
     
-    Meteor.users.update(
-      {_id: Meteor.userId() },
-      {$push: {"profile.fav_personne": this._id}}
-    )
-    alert("Votre profil est maintenant liké")
     },
 
   })
