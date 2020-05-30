cde17090e14814b63905c5c8d2dfd8d7a6aee231
Tinto13 <jacinto.fernandez@unil.ch>
2020-05-30 12:58:53 +0200

    Update page Favoris
    Contrôle qu'une annonce ne peut être likée qu'une fois

--- imports/ui/templates/accannonce_grid.js
+++ imports/ui/templates/accannonce_grid.js
@@ -3,14 +3,21 @@ import { Annonces } from '../../api/annonces';
 import './accannonce_grid.html';
 
 Template.newann.events({
-  'click .ajoutay1'() {
+  
+    'click.ajoutay1'() {
+      if (Meteor.user().profile.fav_annonce.includes(this._id)){
+        alert("Vous avez déjà liké cette annonce");
+  
+      }  else {
+          Meteor.users.update({_id: Meteor.userId() },
+          {$push: {"profile.fav_annonce": this._id}}
+          );
+  
+          alert("Votre annonce est maintenant likée");
+  
+      }
+
     
-    
-    Meteor.users.update(
-        {_id: Meteor.userId() },
-        {$push: {"profile.fav_annonce": this._id}}
-      )
-    alert("Votre annonce est maintenant likée")
     },
 
 })
